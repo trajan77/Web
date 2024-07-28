f428f178878e57c2e1b21ff7d2742c365038e636
231250101 <771206258@qq.com>
2024-07-28 21:24:40 +0800

    前端

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import "./home.css"
+import Form from "../form/Form.jsx";
+
+const Home = () => {
+    return (
+        <div className="back">
+            <img src="../../../public/nothing.png" alt="背景"></img>
+        </div>
+
+    )
+}
+export default Home
---
+++ frontend/src/components/project/Project.jsx
@@ -0,0 +1,16 @@
+import Road1 from "./Road1.jsx";
+import Road2 from "./Road2.jsx";
+import Road3 from "./Road3.jsx";
+import "./project.css"
+
+const Project = () => {
+    return (
+        <div className="project">
+            <Road1/>
+            <Road2/>
+            <Road3/>
+        </div>
+    )
+}
+
+export default Project
